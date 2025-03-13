d92dde4dd8cccc6cd0b57947956c3d017e0c3fa7
Ushapramod <ushanischay2016@gmail.com>
2025-03-13 21:55:59 +0530

    Modify loginfeature.java in loginfeature branch

--- src/test/java/projecr/loginfeature.java
+++ src/test/java/projecr/loginfeature.java
@@ -1,10 +1,10 @@
 package projecr;
 
 public class loginfeature {
-public static void main(String[] args) {
-	System.out.println("nisvhay");
-	System.out.println("vinoda");
-	System.out.println("nisvhay");
-	System.out.println("vinoda");
-}
+    public static void main(String[] args) {
+        System.out.println("nisvhay");
+        System.out.println("vinoda");
+        System.out.println("This line is from the loginfeature branch"); // Conflict line
+        System.out.println("vinoda");
+    }
 }
