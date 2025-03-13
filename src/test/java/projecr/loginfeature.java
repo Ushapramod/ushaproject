bef99a14a7593a7990c2a63ce55be7dd53f1de04
Ushapramod <ushanischay2016@gmail.com>
2025-03-13 13:05:40 +0530

    Second and third print statements

--- src/test/java/projecr/loginfeature.java
+++ src/test/java/projecr/loginfeature.java
@@ -3,5 +3,7 @@ package projecr;
 public class loginfeature {
 public static void main(String[] args) {
 	System.out.println("usha");
+	System.out.println("usha");
+	System.out.println("vinoda");
 }
 }
