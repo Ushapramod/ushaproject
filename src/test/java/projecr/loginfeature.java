12d99fc68a46a73d571a9897dec6e81d8975cb33
Ushapramod <ushanischay2016@gmail.com>
2025-03-13 13:11:27 +0530

    thirst print

--- src/test/java/projecr/loginfeature.java
+++ src/test/java/projecr/loginfeature.java
@@ -5,5 +5,8 @@ public static void main(String[] args) {
 	System.out.println("usha");
 	System.out.println("usha");
 	System.out.println("vinoda");
+	System.out.println("usha");
+	System.out.println("usha");
+	System.out.println("vinoda");
 }
 }
