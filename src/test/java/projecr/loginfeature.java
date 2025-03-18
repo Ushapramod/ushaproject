694e3a070dda4e334775793505f00171c33a5c1b
Ushapramod <97018722+Ushapramod@users.noreply.github.com>
2025-03-18 22:19:11 +0530

    Update loginfeature.java

--- src/test/java/projecr/loginfeature.java
+++ src/test/java/projecr/loginfeature.java
@@ -1,6 +1,10 @@
 package projecr;
 
 public class loginfeature {
+    public void print()
+	{
+		System.out.println("My name is usha");
+	}
     public static void main(String[] args) {
         System.out.println("nisvhay");
         System.out.println("vinoda");
